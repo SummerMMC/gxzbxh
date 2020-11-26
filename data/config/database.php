492b03d964653e71d8a6cf60cce1bd3cbbb2d28d
Songning Liu <songningliu@SongningdeMacBook-Pro.local>
2020-11-26 16:38:12 +0800

    data提交

---
+++ data/config/database.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * 配置文件
+ */
+
+/**
+ * 本地
+ */
+//return [
+//    // 数据库类型
+//    'type'     => 'mysql',
+//    // 服务器地址
+//    'hostname' => 'localhost',
+//    // 数据库名
+//    'database' => 'zbxh',
+//    // 用户名
+//    'username' => 'root',
+//    // 密码
+//    'password' => 'root',
+//    // 端口
+//    'hostport' => '3306',
+//    // 数据库编码默认采用utf8
+//    'charset'  => 'utf8mb4',
+//    // 数据库表前缀
+//    'prefix'   => 'cmf_',
+//    "authcode" => 'uGoqyHmWLjApNG8ZSR',
+//    //#COOKIE_PREFIX#
+//];
+
+/**
+ * 线上
+ */
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '42.194.150.217',
+    // 数据库名
+    'database' => 'zbxh',
+    // 用户名
+    'username' => 'zbxh',
+    // 密码
+    'password' => 'Mimashiwo7788dd',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'   => 'cmf_',
+    "authcode" => 'uGoqyHmWLjApNG8ZSR',
+    //#COOKIE_PREFIX#
+];
---
+++ data/route/route.php
@@ -0,0 +1,25 @@
+<?php	return array (
+  '啊/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=7',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+      'cid' => '\\d+',
+    ),
+  ),
+  '啊' => 
+  array (
+    0 => 'portal/List/index?id=7',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+);
